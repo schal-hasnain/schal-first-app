ec2db1bdb5efed42d25c93eeebc5647fc25e4c33
Schal Hasnain <63582840+schal-hasnain@users.noreply.github.com>
2020-09-23 07:51:19 +0500

    80%

--- script.js
+++ script.js
@@ -1,16 +1,49 @@
-function enterpressed(){
-    var newinput = document.getElementById("newelement").value;
-    var list = document.getElementById("list");
-    var newlistmember = document.createTextNode(newinput);
-    var newLi = document.createElement("li");
-    newLi.appendChild(newlistmember);
-    list.appendChild(newLi);
+$(function resetclicked(){
+    document.getElementById("update").disabled = true;
+   $("#reset").click(function handleresetbtn(){
+       if(($("#name").val()== "")&&($("#age").val()=="")&&($("#city").val()==""))
+       {
+           alert("Already Empty!!");
+       }
+       $("#name").val("");
+       $("#age").val("");
+       $("#city").val("");
+       $( "#male" ).prop( "checked", false );
+       $( "#female" ).prop( "checked", false );
+       document.getElementById("add").disabled = false;
+       document.getElementById("update").disabled = true;
+      
+
+   }) 
+});
+$(function addclicked(){
+    $("#add").click(function handleaddbtn(){
+        if(($("#name").val()== "")||($("#age").val()=="")||($("#city").val()==""))
+        {
+            alert("Fill Every Field!!")
+            return;
+        }
+        var name = $("#name").val();
+        if ($("#male").prop("checked"))
+        {
+            var gender = "Male";
+        }
+        else{
+            var gender = "Female";
+        }
+        var age = $("#age").val();
+        var city = $("#city").val();
+        var newRow = "<tr><td>" + name + "</td><td>" + gender + "</td><td>"+age+"</td><td>"+city+"</td><td><button onclick = deleteclicked() id=delete>Delete</button><button onclick = updateclicked() id = update1>Update</button></td></tr>";
+        $("#infoTable").append(newRow);
+    })
+    
+   
+});
+function deleteclicked(){
+$("#delete").parent().parent().remove();
+}
+function updateclicked(){
+    document.getElementById("add").disabled = true;
+    document.getElementById("update").disabled = false;
 }
 
-
-function deletecalled(e){
-    var tag = e.target;
-    var li = tag.parentNode;
-    li.parentNode.removeChild(li);
-
-}
