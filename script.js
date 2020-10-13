4109e98816d3d628a50d4269d11943d26118c78c
Schal Hasnain <63582840+schal-hasnain@users.noreply.github.com>
2020-10-13 05:54:18 +0500

    Task 1 Completed
    Task 1 Completed

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ $(function addclicked(){
         }
         var age = $("#age").val();
         var city = $("#city").val();
-        var newRow = "<tr><td>" + name + "</td><td>" + gender + "</td><td>"+age+"</td><td>"+city+"</td><td><button onclick = deleteclicked() id=delete>Delete</button><button onclick = updateclicked() id = update1>Update</button></td></tr>";
+        var newRow = "<tr><td>" + name + "</td><td>" + gender + "</td><td>"+age+"</td><td>"+city+"</td><td><button onclick = deleteclicked() id=delete>Delete</button><button type=button class='btn btn-success btn-edit'>Update</button></td></tr>";
         $("#infoTable").append(newRow);
     })
     
@@ -46,10 +46,55 @@ $(function addclicked(){
 function deleteclicked(){
 $("#delete").parent().parent().remove();
 }
-function updateclicked(){
+var req_tr=null;
+$(document).on('click', '.btn-edit', function(){
     document.getElementById("add").disabled = true;
     document.getElementById("update").disabled = false;
-      
-}
+    req_tr = $(this).closest('tr');
+    var new_name = $(req_tr).find('td:eq(0)').text();
+    var new_gender = $(req_tr).find('td:eq(1)').text();
+    var new_age = $(req_tr).find('td:eq(2)').text();
+    var new_city = $(req_tr).find('td:eq(3)').text();
+    $("#name").val(new_name);
+    $("#age").val(new_age);
+    $("#city").val(new_city);
+    if (new_gender == "Male")
+    {
+        $( "#male" ).prop( "checked", true );
+    }
+    else{
+        $( "#female" ).prop( "checked", true );
+    }
+    $("#update").click(function updatebtnhandle(){
+        if(($("#name").val()== "")||($("#age").val()=="")||($("#city").val()==""))
+        {
+            alert("Fill Every Field!!")
+            return;
+        }
+        var name = $("#name").val();
+        if ($("#male").prop("checked"))
+        {
+            var gender = "Male";
+        }
+        else{
+            var gender = "Female";
+        }
+        var age = $("#age").val();
+        var city = $("#city").val();
+        if(req_tr)
+        {
+        $(req_tr).find('td:eq(0)').text(name);
+        $(req_tr).find('td:eq(1)').text(gender);
+        $(req_tr).find('td:eq(2)').text(age);
+        $(req_tr).find('td:eq(3)').text(city);
+        alert("Record Updated !!");
+        req_tr = null;
+        }
+    });
+
+});
+
+
+
 
 
